9995951f9d40b388b7cf3796e1ebc2693b102e6a
Ashutosh Kumar <akumar1@vt.edu>
2017-09-19 13:15:28 -0400

    input file for properties, fixed some file already open issues

--- transform.cc
+++ transform.cc
@@ -14,6 +14,7 @@ void transform_to_mo(SharedWavefunction ref_wfn, std::shared_ptr<PSIO> psio)
     spaces.push_back(MOSpace::occ);
     spaces.push_back(MOSpace::vir);
     IntegralTransform ints(ref_wfn, spaces, IntegralTransform::Restricted, IntegralTransform::DPDOnly);
+    ints.set_keep_iwl_so_ints(true);
     ints.transform_tei(MOSpace::occ, MOSpace::vir, MOSpace::occ, MOSpace::vir);
     // Use the IntegralTransform object's DPD instance, for convenience
     dpd_set_default(ints.get_dpd_id());
@@ -25,6 +26,7 @@ void transform_to_mo(SharedWavefunction ref_wfn, std::shared_ptr<PSIO> psio)
     global_dpd_->buf4_sort(&K, PSIF_CC_DINTS, prqs, ID("[O,O]"), ID("[V,V]"), "D <ij|ab>");
     global_dpd_->buf4_close(&K);
     psio->close(PSIF_LIBTRANS_DPD, 1);
+    psio->close(PSIF_CC_DINTS, 1);
 
  }
 
